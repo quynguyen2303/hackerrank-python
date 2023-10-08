6b29ef73cf9a0be22a6841cb9fa3cf4f33f4a3db
quynguyen2303 <nguyenphuquy2303@gmail.com>
2023-10-08 11:50:32 +0700

    add itertools

--- combinations.py
+++ combinations.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from itertools import combinations_with_replacement
 
 string, k = input().split(" ")
 k = int(k)
@@ -8,3 +9,6 @@ string = "".join(sorted([c for c in string]))
 for i in range (1, k+1):
     for c in list(combinations(string, i)):
         print("".join(c))
+
+for c in list(combinations_with_replacement(string, k)):
+    print("".join(c))
